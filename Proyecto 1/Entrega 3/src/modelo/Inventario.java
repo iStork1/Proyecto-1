2c0724bacddfee18a95513146035f1ba308c9705
ju4ndiegos <jd.sanchez2>
2023-10-15 20:03:48 -0500

    avance control reservas y alquiler

---
+++ Proyecto 1/Entrega 3/src/modelo/Inventario.java
@@ -0,0 +1,45 @@
+package modelo;
+
+import java.util.*;
+
+public class Inventario {
+	Map<String, List<Vehiculo>> Vehiculos = new HashMap<>();
+	
+	private Vehiculo getVehiculo(String placa) {
+		for (Map.Entry<String, List<Vehiculo>> entrada : Vehiculos.entrySet()) {
+		    String sede = entrada.getKey();
+		    List<Vehiculo> listaDeVehiculos = entrada.getValue();
+		    System.out.println("Sede: " + sede);
+		    for (Vehiculo vehiculo : listaDeVehiculos) {
+                if (vehiculo.getPlaca().equals(placa)){
+                	Vehiculo vehiculoEncontrado=vehiculo;
+                	
+                }
+            }
+        }
+		
+	}
+	public Registro getLogVehiculo (String placa) {
+		
+	}
+	public void cambiarSedeVehiculo() {
+		
+	}
+	public void agregarVehiculo(Vehiculo vehiculo) {
+		if (Vehiculos.containsKey(vehiculo.getnombreSede()) {
+            // Si la sede ya existe, obtener la lista de vehículos asociada
+            List<Vehiculo> listaDeVehiculos = Vehiculos.get(nombreSede);
+            listaDeVehiculos.add(vehiculo);
+	 }
+		else {
+			List<Vehiculo> nuevaListaDeVehiculos = new ArrayList<>();
+            nuevaListaDeVehiculos.add(vehiculo);
+            Vehiculos.put(Vehiculo.getsedeUbicado().getnombreSede(), nuevaListaDeVehiculos);
+		}
+	}
+	public void eliminarVehiculo(String placa) {
+		
+	}
+	
+		
+}
